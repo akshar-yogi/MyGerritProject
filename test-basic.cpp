2eb16386b5d1318561f5b3695e282e774222ef8d
Akshar Yogi <akshar.v.yogi@gmail.com>
2022-12-09 15:58:15 +0530

    Initial commit.

---
+++ test-basic.cpp
@@ -0,0 +1,91 @@
+#define XYZE
+
+///////////////////////////////////////////////////////////////////////////////
+
+#ifdef XYZE
+
+#include <iostream>
+#include <string>
+#include <cstring>
+
+// Abstract base class as it contains pure virtual functions. It can not be 
+// instantiated.
+struct IMyInterface { 
+    virtual ~IMyInterface() = default; // Virtual destructor
+    virtual std::string Id() const = 0; // Pure virtual function
+    virtual void Id(const std::string&) {}; // Not a pure virtual function
+};
+
+// Derived class using virtial inheritance and public member access specifier
+class MyClass : public virtual IMyInterface {
+private:
+    MyClass() = delete; // Explicitly deleted constructor
+    MyClass(const MyClass&) = delete; // Deleted copy constructor
+    MyClass& operator=(const MyClass&) = delete; // Deleted assignment operator
+
+public:
+    explicit MyClass(const std::string& id)
+        : _id(id)
+    {
+    }
+    ~MyClass() override = default;
+
+public:
+    std::string Id() const override
+    {
+        return (_id);
+    }
+    void Id(const std::string& id) override
+    {
+        _id = id;
+    }
+    bool operator==(const MyClass& other) const { // Operator overloading
+        if (strcasecmp(Id().c_str(), other.Id().c_str()) == 0) { // compare ignoring case
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // Factory method to create singleton
+    static MyClass& Create()
+    {
+        static MyClass instance("DefaultString");
+        return instance;
+    }
+
+private:
+    std::string _id;
+};
+
+int main()
+{
+    // Object creation / Operator overloading
+    {
+        MyClass mc1("MyString");
+        std::cout << mc1.Id() << "\n";
+
+        MyClass mc2("mystring");
+        if (mc1 == mc2) { // Operator overloading
+            std::cout << "Strings are same\n";
+        } else {
+            std::cout << "Strings are not same\n";
+        }
+    }
+
+    // Explicitly deleted or defaulted functions
+    {
+        // MyClass mc1; // Not allowed as default constructor is deleted. 
+        // MyClass mc2(mc1); // Not allowed as copy constructor is deleted
+        // mc1 = MyClass(); // Now allowed as assignment operator is deleted
+    }
+
+    // Using Factory method to create singleton
+    {
+        // MyClass& mc = MyClass::Create();
+        // std::cout << mc.Id() << "\n";
+    }
+
+    return (0);
+}
+
+#endif
